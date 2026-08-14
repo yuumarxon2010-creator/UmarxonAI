bd283308ecc96c18a7736388a57a2cbf4374c711
yuumarxon2010-creator <y.u.umarxon2010@gmail.com>
2026-08-14 20:57:00 +0500

    Update build.gradle.kts plugin configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
 plugins {
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
 }
