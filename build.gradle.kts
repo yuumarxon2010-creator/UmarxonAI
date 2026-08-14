12dc4e91564b2e0fb8fd18cd34c7b46a4a88f9dc
yuumarxon2010-creator <y.u.umarxon2010@gmail.com>
2026-08-14 21:47:06 +0500

    Replace KSP plugin ID with alias in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
+    alias(libs.plugins.google.devtools.ksp) apply false
 }
