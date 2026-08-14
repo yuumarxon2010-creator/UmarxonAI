2c1aa0beec8108b410132922436f7e84afcf9402
yuumarxon2010-creator <y.u.umarxon2010@gmail.com>
2026-08-14 21:54:10 +0500

    Fix typo in plugin alias for roborazzi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.devtools.ksp)
-    alias(libs.plugins.roboazzi)
+    alias(libs.plugins.roborazzi)
     alias(libs.plugins.secrets)
     alias(libs.plugins.google.services)
 }
