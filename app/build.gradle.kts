f662e340085a3b904016913757c5fd2de6e63f3d
yuumarxon2010-creator <y.u.umarxon2010@gmail.com>
2026-08-15 11:21:31 +0500

    Remove debug signing configuration
    Comment out debug signing configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,7 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
-  }
+    // debug { signingConfig = signingConfigs.getByName("debugConfig") }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
